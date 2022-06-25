9985321525f6cbf2b7aee7d3931d79e608c474cb
naDiel-D <altarro@o2.pl>
2022-06-25 20:46:54 +0100

    Adding createfile function

--- PSiZ_Z4/PSiZ_Z4.cpp
+++ PSiZ_Z4/PSiZ_Z4.cpp
@@ -39,3 +39,93 @@ void openLog(string fileName) // Otworz log
     }
     saveLog("Plik z logami otwoarto.");
 }
+
+void closeLog(void) // Zamknij log
+{
+    saveLog("Zamknieto plik z logami.");
+    logPlik.close();
+}
+
+void createFile(const string fileName, const int counter, const char values) // Stworzenie pliku + zmiana 10 bitów dla drugiego testu
+{
+    srand(time(NULL));
+    ifstream myFile;
+
+    const char diffCharacter = 0x35;
+    int lM[10] = {};
+    bool kk = false;
+    int temp = {};
+    int k = 0;
+
+    myFile.open(fileName);
+    if (myFile)
+    {
+        saveLog("Ten plik " + fileName + " juz istnieje!\n");
+    }
+    else {
+
+        ofstream aFile(fileName);
+
+        if (fileName == "t2_plik4.bin") // Zamiana 10 bitow dla pliku 4 dla drugiego testu
+        {
+            for (size_t i = 0; i < 10; i++)
+            {
+                do
+                {
+                    temp = rand() % counter + 1;
+                    for (size_t j = 0; j < 10; j++)
+                    {
+                        if (temp == lM[j]) {
+                            kk = true;
+                        }
+                        else kk = false;
+                    }
+
+                } while (kk);
+                lM[i] = temp;
+            }
+            sort(lM, lM + 10);
+
+            for (size_t i = 0; i < 10; i++)
+            {
+                cout << lM[i] << ", ";
+            }
+
+            for (size_t i = 0; i < 11; i++)
+            {
+                if (i < 10) {
+                    for (k; k < lM[i]; k++)
+                    {
+                        aFile << values;
+
+                    }
+
+                    aFile << diffCharacter;
+
+                    k++;
+                }
+                else {
+                    for (k; k < counter; k++)
+                    {
+                        aFile << values;
+                    }
+                }
+            }
+        }
+        else
+        {
+
+            for (size_t i = 0; i < counter; i++)
+            {
+                aFile << values;
+            }
+        }
+
+        string log = "Plik: " + fileName + " zostal stworzony\n";
+
+        saveLog(log);
+        system("cls");
+
+    }
+
+}
